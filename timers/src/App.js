9bcbca8a893d82631625d08d0c1e02c06ad70f02
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2017-07-11 13:18:20 +0300

    master: cssnext, react-toolbox

--- timers/src/App.js
+++ timers/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'react-toolbox/lib/button';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+	    <Button label="Hello World!" />
       </div>
     );
   }
